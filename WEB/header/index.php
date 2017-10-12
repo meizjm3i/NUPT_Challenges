e776f1ab52eccd920ba48011928eb52831b4d654
梅子酒 <960596293@qq.com>
2017-10-12 21:53:45 +0800

    Update index.php

--- WEB/header/index.php
+++ WEB/header/index.php
@@ -1,1 +1,15 @@
 
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<?php
+header("Flag:nctf{tips_often_hide_here}");
+echo "<pre>";
+echo "<br><br>";
+echo "<br><br>";
+
+echo "什么也没有。<br><br>";
+
+?>
+
+</html>
